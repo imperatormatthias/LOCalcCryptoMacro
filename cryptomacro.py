6248f3c4bfc9c20bd1b7d624c8c09b05384289b7
mateuszrb <childrenofdecadence@gmail.com>
2017-09-14 15:04:05 -0400

    changed code to look for symbol in first column

--- cryptomacro.py
+++ cryptomacro.py
@@ -4,19 +4,17 @@ import urllib.request
 
 oDoc = XSCRIPTCONTEXT.getDocument()
 
-coins = ['bitcoin', 'ethereum', 'ripple']
-
 with urllib.request.urlopen('https://api.coinmarketcap.com/v1/ticker/') as req:
     req = req.read()
 
 response = json.loads(req.decode())
 
 def getPrices():
-    for i in range(len(response)):
-        for j in range(len(coins)):
-            if response[i]['id'] == coins[j]:
-                oSheet = oDoc.CurrentController.ActiveSheet
-                oCell1 = oSheet.getCellByPosition(0,j+1)
-                oCell1.String = response[i]['symbol']
-                oCell2 = oSheet.getCellByPosition(1,j+1)
-                oCell2.Value = response[i]['price_usd']
+    oSheet = oDoc.CurrentController.ActiveSheet
+    for i in range(1,20):
+        oCell1 = oSheet.getCellByPosition(0,i)
+
+        for j in range(len(response)):
+            if oCell1.String == response[j]['symbol']:
+                oCell2 = oSheet.getCellByPosition(1,i)
+                oCell2.Value= response[j]['price_usd']
